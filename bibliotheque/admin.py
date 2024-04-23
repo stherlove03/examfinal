3adda3f515a4313fa30ad4b288f882d36b00dc69
Esther Love Prisnie Desravines <prisnie11@gmail.com>
2024-04-23 12:58:33 -0400

    correction erreur

--- bibliotheque/admin.py
+++ bibliotheque/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Livre
 # Register your models here.
+class affichagelivre(admin.ModelAdmin):
+    list_display = ['code','titre','auteur','annee_publication', 'genre']
+
+admin.site.register(Livre , affichagelivre)
